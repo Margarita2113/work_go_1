e3630c63b52e266005e623d3ed5489a4693c8da7
Margarita2113 <rita_2113@mail.ru>
2025-03-08 15:47:51 +0300

    done

--- main.go
+++ main.go
@@ -105,19 +105,19 @@ func errorResponce() {
 
 func (d Data) check() {
 	if d.LoadAverage > 30 {
-		fmt.Println(fmt.Sprintf("Load Average is too high: %d", d.LoadAverage))
+		fmt.Printf("Load Average is too high: %d/n", d.LoadAverage)
 	}
 	percentRamLoad := d.RAMUsage * 100 / d.RAM
 	if percentRamLoad > 80 {
-		fmt.Println(fmt.Sprintf("Memory usage too high: %d", percentRamLoad) + "%")
+		fmt.Printf("Memory usage too high: %d%/n", percentRamLoad)
 	}
 	percentRomLoad := d.ROMUsage * 100 / d.ROM
 	if percentRomLoad > 90 {
-		fmt.Println(fmt.Sprintf("Free disk space is too low: %d Mb left", (d.ROM-d.ROMUsage)/(1024*1024)))
+		fmt.Println("Free disk space is too low: %d Mb left/n", (d.ROM-d.ROMUsage)/(1024*1024))
 	}
 	percentNetwork := d.NetworkUsage * 100 / d.Network
 	if percentNetwork > 90 {
-		fmt.Println(fmt.Sprintf("Network bandwidth usage high: %d Mbit/s available",
-			(d.Network-d.NetworkUsage)/(1000*1000)))
+		fmt.Println("Network bandwidth usage high: %d Mbit/s available/n",
+			(d.Network-d.NetworkUsage)/(1000*1000))
 	}
 }
